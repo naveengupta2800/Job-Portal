d97c9e253721db278144e3233c01a3a7e0c5311d
naveengupta2800 <naveengupta.2803@gmail.com>
2025-11-26 22:34:00 +0530

    add job create,job lising and job update api in server.js

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ mongoose.connect(MONGO_URI)
 
 // USER SCHEMA
 const userSchema = new mongoose.Schema({
-  name: String,
-  password: String,
+  name: String, 
+  password: String,  
   email: { type: String, unique: true },
   role: { type: String, enum:['jobseeker', 'recruiter'], required: true },
 
@@ -46,7 +46,7 @@ const userSchema = new mongoose.Schema({
   otpExpire: Date,
   isVerified: { type: Boolean, default: false },
 
-  //profile
+  //profile user
   phone:String,
   address:String,
   skills: [String],
@@ -64,11 +64,30 @@ const userSchema = new mongoose.Schema({
       years: String
     }
   ],
+  company:String,
+  companyWebsite:String,
+  totalJobsPosted: { type: Number, default: 0 }
  },
  {
   timestamps: true 
 });
 const User = mongoose.model('User', userSchema);
+
+const JobSchema = new mongoose.Schema({
+  recruiterId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  location: { type: String, required: true },
+  company: { type: String, required: true },
+  salary: { type: String },
+  experience:{type: String},
+  skillsRequired: { type: [String], required: true },
+  employmentType: { type: String, enum: ['Full-Time', 'Part-Time', 'Internship', 'Remote', 'Contract'], required: true },
+  createdAt: { type: Date, default: Date.now }
+},{timestamps:true});
+
+const Job = mongoose.model("Job", JobSchema);
+
 
 // Nodemailer
 const transporter = nodemailer.createTransport({
@@ -181,13 +200,22 @@ app.post('/login', async(req, res) => {
     req.session.token = token;
     req.session.userId = user.id;
 
-    res.json({ message: 'Logged in successfully' });
+    req.session.save((err) => {
+      if (err) {
+        console.log("Session save error:", err);
+        return res.status(500).json({ message: "Session save failed" });
+      }
+
+      res.json({
+        message: 'Logged in successfully'
+      });
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: 'Server error' });
   }
 });
-app.get('/profile',async(req,res)=> {
+app.get('/profile/jobseeker',async(req,res)=> {
   try {
       const token = req.session.token;
       if(!token){
@@ -222,13 +250,12 @@ app.get('/profile',async(req,res)=> {
         res.status(500).json({ message: 'Server error' });
   }
 });
-app.put('/profile-update', async(req,res) => {
+app.put('/profile/jobseeker/update', async(req,res) => {
   try {
     const token = req.session.token;
     if (!token) {
       return res.status(401).json({ message: 'Unauthorized user' });
     }
-
     const decoded = jwt.verify(token, JWT_SECRET);
     const userId = decoded.userId;
 
@@ -263,8 +290,184 @@ app.put('/profile-update', async(req,res) => {
     return res.status(500).json({ message: 'Server error' });
   }
 });
+app.get('/profile/recruiter',async(req,res)=> {
+    try{
+        const token = req.session.token;
+        if(!token){       //Pehle check karta hai user login hai ya nahi
+          return res.status(401).json({ messaage:'Unauthorized user' });
+        }
+        const decoded= jwt.verify(token,JWT_SECRET); // Fir JWT token decode karta hai
+        const user = await User.findById(decoded.userId).select("-password -otp -otpExpire");  //Fir JWT token decode karta hai
+        if(!user){ //Fir agar user exist nahi hai to error deta hai
+          return res.status(404).json({message:"Recuiter not found"});
+        }
+        if(user.role!=="recruiter"){
+          return res.status(403).json({message:'Access Denied, not a recruiter'});
+        }
+       return res.json({
+        message: 'Recruiter Fetched successfully',
+        profile: {
+          name: user.name,
+          email:user.email,
+          phone:user.phone||null,
+          company:user.company || null,
+          companyWebsite: user.companyWebsite || null,
+          address: user.address|| null,
+          role:user.role || null,
+          createdAt: user.createdAt || null,
+          totalJobsPosted: user.totalJobsPosted || null
+        }
+       });
+    }
+    catch(err){
+      console.log(err);
+      return res.status(500).json({message:'Server error'});
+    }
+})
+app.put('/profile/recruiter/update',async(req,res)=> {
+   try{
+  
+        const token = req.session.token;
+        if(!token){       //Pehle check karta hai user login hai ya nahi
+          return res.status(401).json({ messaage:'Unauthorized user' });
+        }
+        const decoded= jwt.verify(token,JWT_SECRET); // Fir JWT token decode karta hai
+        const user = await User.findById(decoded.userId).select("-password -otp -otpExpire");  //Fir JWT token decode karta hai
+        if(!user){ //Fir agar user exist nahi hai to error deta hai
+          return res.status(404).json({message:"Recuiter not found"});
+        }
+        if(user.role!=="recruiter"){
+          return res.status(403).json({message:'Access Denied, not a recruiter'});
+        }
+        const allowed = ['name','email','phone','company','companyWebsite','address','totalJobsPosted']
 
-
+        const updates= {};
+        for(const key in req.body){
+          if(allowed.includes(key)) {
+            updates[key] = req.body[key];
+          }
+        }
+        const updatedUser = await User.findByIdAndUpdate(decoded.userId,
+          {$set:updates},
+          {new: true}
+        ).select("-password -otp -otpExpire");
+        res.json({message: 'Profile updated successfully', profile: updatedUser})
+      }
+  
+  catch(err){
+    console.log(err);
+    return res.status(500).json({message:'Server error'});
+  }
+});
+app.post('/job/create',async(req,res)=> {
+  try {
+     const token = req.session.token;
+        if(!token){       
+          return res.status(401).json({ messaage:'Unauthorized user' });
+        }
+        const decoded= jwt.verify(token,JWT_SECRET); 
+        const user = await User.findById(decoded.userId).select("-password -otp -otpExpire");
+        if(!user){
+          return res.status(404).json({message:"User not found"});
+        }
+        if(user.role!=="recruiter"){
+          return res.status(403).json({message:'Access Denied, recruiter can post jobs'});
+        }
+        const{title,description,location,company,salary,experience,skillsRequired,employmentType} = req.body;
+        
+        if(!title || !description){
+          return res.status(400).json({message:'Title and Description are required'});
+        }
+        if(!company || !location){
+          return res.status(400).json({message:'Company and Location are required'});
+        }
+        
+        const newJob = new Job({
+          recruiterId: user._id,
+          title,
+          description,
+          location,
+          company,
+          salary,
+          experience,
+          skillsRequired,
+          employmentType
+        });
+        await newJob.save();
+      res.status(201).json({
+      message: 'Job created successfully',
+      job: newJob })
+      }
+      catch(err){
+        console.log(err);
+        return res.status(500).json({message:'Internal Server error'});
+      }
+  
+});
+app.get('/job/all', async(req,res)=> {
+  try {
+    const token = req.session.token;
+    if(!token){
+      return res.status(401).json({ messaage:'Unauthorized user' });
+    } 
+    const decoded = jwt.verify(token,JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password -otp -otpExpire");
+    if(!user){
+      return res.status(404).json({message: "User not found"});
+    }
+    const jobs = await Job.find();
+    return res.json({
+      message:" See All Jobs ",
+      totalJobs:jobs.length,
+      jobs:jobs
+    });
+  }
+  catch(err){
+    console.log(err);
+    return res.status(500).json({message:"Server error"});
+  }
+});
+app.put('/jobs/update/:jobId',async(req,res)=> {
+  try {
+    const token = req.session.token;
+    if(!token){
+      return res.status(401).json({
+        message:"Unautorized User"
+      });
+    }
+    const decoded = jwt.verify(token,JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password -otp -otpExpire");
+    if(!user) { return res.status(404).json({message: "User not found"});
+  }
+  if(user.role !== "recruiter") {
+     return res.status(403).json({message:'Access Denied, not a recruiter'});
+  }
+  const jobId = req.params.jobId;
+    if (!jobId) {
+      return res.status(400).json({ message: "Job ID is required" });
+    }
+    const allowed = ['title','description','location','company','salary','experience','skillsRequired','employmentType'];
+    const updates = {};
+    for(const key in req.body){
+      if(allowed.includes(key)){
+        updates[key] = req.body[key];
+      }
+    }
+      const UpdateJob = await Job.findByIdAndUpdate(jobId,{$set:updates},
+        {new:true}
+      );
+      if (!UpdateJob) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+      res.json({message:'Job Update Successfully',
+         UpdateJob
+      })
+    }
+  catch(err){
+    console.log(err);
+    return res.status(500).json({message:"Server error"});
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
